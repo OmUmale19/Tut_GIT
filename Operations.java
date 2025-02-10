76e703ed8b32c8e4edc84a5b4843bb1b6658a6ed
OmUmale19 <146768439+OmUmale19@users.noreply.github.com>
2025-02-10 16:10:52 +0530

    All features added

--- Operations.java
+++ Operations.java
@@ -1,23 +1,49 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Operations{
-  public static void main(String[] args){
-    Scanner scanner = new Scanner(System.in);
+
+  static int add(int a,int b){
+    return a+b;
+  }
+  static int subt(int a,int b){
+    return a-b;
+  }
+  static int mul(int a,int b){
+    return a*b;
+  }
+  static int div(int a,int b){
+    return a/b;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     
-        // Taking input from the user
-        System.out.print("Enter first number: ");
-        int num1 = scanner.nextInt();
+    System.out.println("Enter first number");
+    int a = sc.nextInt();
+    System.out.println("Enter second number");
+    int b = sc.nextInt();
 
-        System.out.print("Enter second number: ");
-        int num2 = scanner.nextInt();
+    while (true) {
+      System.out.println("Enter the option: \n1. Add\n2. Sub\n3. Mul\n4. Div");
+      int option = sc.nextInt();
 
-        int sum = num1 + num2; // Adding the numbers
-
-        int subt = num1 - num2; // Subtracting the numbers
-
-        System.out.println("The sum is: " + sum); // Display the result
-        System.out.println("The Difference is: " + subt); // Display the result;
-
-        scanner.close(); // Close the scanner
+      switch (option) {
+        case 1:
+            System.out.println("Addition is: "+add(a, b));
+            break;
+        case 2:
+            System.out.println("Subtraction is: "+subt(a, b));
+            break;
+        case 3:
+            System.out.println("Multiplication is: "+mul(a, b));
+            break;
+        case 4:
+            System.out.println("Division is: "+div(a, b));
+            break;
+        default:
+            System.out.println("Select valid option");
+    }
+    }
+    
   }
 }
